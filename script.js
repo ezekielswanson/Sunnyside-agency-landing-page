baa5eb4ddef16fa2ebf59c8baddff8fb85a3e820
ezekielswanson <swansonezekiel@gmail.com>
2021-12-30 21:28:22 -0600

    Updated html, css, and javascript files.

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 
 /*GLOBAL VARIABLES */
 
+const menuBtn = document.getElementById('nav-toggle');
+
+function openMobileMenu () {
+    menuBtn.classList.toggle('hidden');
+}
+
+menuBtn.addEventListener('click', openMobileMenu)
